a353f56c04136e47a7d98c2d1658126e0828cf9e
zaq_pc <zhang_anqing@126.com>
2020-12-21 17:25:41 +0800

    commit a new .py file

---
+++ new.py
@@ -0,0 +1,37 @@
+
+import os
+import numpy as np
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torchvision
+from torchvision import transforms
+import ignite
+from ignite.engine import create_supervised_evaluator
+
+#import backdoor_attack as bd
+import backdoor_attack.plot_util
+import net
+
+#%%
+
+class Dataset_from_array(torch.utils.data.Dataset):
+    def __init__(self, x, t, transform=None):
+        self.transform = transform
+        self.x = x
+        self.t = t
+        self.num = x.shape[0]
+
+    def __len__(self):
+        return self.num
+
+    def __getitem__(self, idx):
+        x = self.x[idx]
+        t = self.t[idx]
+
+        if not self.transform is None:
+            x = self.transform(x)
+
+        return x, t
+
